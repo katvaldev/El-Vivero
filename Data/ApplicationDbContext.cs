14a37ab600293eefaf1368340ff3dbf3dbad34fd
Kat <kat.valcarcel@live.com>
2020-11-21 02:03:43 -0500

    cambio carpeta

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vivero.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Vivero.Models.Carrito> Carrito { get; set; }
+
+        public DbSet<Vivero.Models.Plaga> Plaga { get; set; }
+
+        public DbSet<Vivero.Models.Planta> Planta { get; set; }
+    }
+}
