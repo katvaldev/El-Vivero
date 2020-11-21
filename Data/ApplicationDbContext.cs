0b0b0abaf016fb569693869c2511ae9783d84b51
KatV1 <kat.valcarcel@live.com>
2020-11-21 08:04:22 -0500

    ef installed

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,10 +12,10 @@ namespace Vivero.Data
             : base(options)
         {
         }
-        public DbSet<Vivero.Models.Carrito> Carrito { get; set; }
+        // public DbSet<Vivero.Models.Carrito> Carrito { get; set; }
 
-        public DbSet<Vivero.Models.Plaga> Plaga { get; set; }
+        // public DbSet<Vivero.Models.Plaga> Plaga { get; set; }
 
-        public DbSet<Vivero.Models.Planta> Planta { get; set; }
+        // public DbSet<Vivero.Models.Planta> Planta { get; set; }
     }
 }
