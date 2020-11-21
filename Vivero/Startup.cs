c28dec629920e401a57e0cf129724f9a6afc3e54
KatV1 <kat.valcarcel@live.com>
2020-11-21 01:28:50 -0500

    DB

--- Vivero/Startup.cs
+++ Vivero/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 namespace Vivero
 {
     public class Startup
@@ -27,9 +29,15 @@ namespace Vivero
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            // services.AddDbContext<ApplicationDbContext>(options =>
+            //     options.UseSqlite(
+            //         Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseNpgsql(
+                    Configuration.GetConnectionString("postgresql-db")
+                ));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
