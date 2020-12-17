6af6dd641596b09d381337f2bbd8b9646321ba12
jair-felix <jair_alarcon@usmp.pe>
2020-12-17 18:24:35 -0500

    asd

--- Models/Plaga.cs
+++ Models/Plaga.cs
@@ -11,35 +11,22 @@ namespace Vivero.Models
         
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("IDplaga")]
-        public int IDplaga { get; set; }
+        [Column("id")]
+        public int id { get; set; }
+
+
+        [Column("nombreplaga")]
+        public string nombreplaga { get; set; }
+
+
+        [Column("descripcion")]
+        public string descripcion { get; set; }
 
 
 
-        [Column("Descripcion")]
-        public string Descripcion { get; set; }
+        [Column("adicional")]
+        public string adicional { get; set; }
 
-
-
-        [Column("nombrePlaga")]
-        public string nombrePlaga { get; set; }
-
-
-
-        [Column("Adicional")]
-        public string Adicional { get; set; }
-
-
-        [Column("imageplaga")]
-        public string imageplaga { get; set; }
-
-
-        
-        
-
-
-        [NotMapped]
-        public string Respuesta { get; set; }
 
     }
 }
