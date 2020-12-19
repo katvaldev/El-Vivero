dae9f026dcfeabb452a791e5fe37380ce1e2856c
jair-felix <jair_alarcon@usmp.pe>
2020-12-18 23:44:39 -0500

    Merge branch 'Plagas' of https://github.com/KatV1/El-Vivero into Plagas

--- Controllers/PlagasController.cs
+++ Controllers/PlagasController.cs
@@ -18,12 +18,14 @@ namespace Vivero.Controllers
 
         private readonly ILogger<PlagasController> _logger;
          private readonly ApplicationDbContext _context;
+         
 
         public PlagasController(ILogger<PlagasController> logger,
         ApplicationDbContext context)
         {
             _logger = logger;
             _context = context;
+           
         }
 
 
@@ -46,6 +48,52 @@ namespace Vivero.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plaga = await _context.plaga.FindAsync(id);
+            if (plaga == null)
+            {
+                return NotFound();
+            }
+            return View(plaga);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,nombreplaga,descripcion,adicional")] Plaga plaga)
+        {
+            if (id != plaga.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(plaga);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plaga);
+        }
+
+
+
+
 
         public IActionResult Formulario()
         {
